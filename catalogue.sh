abc7bb859f32f63bb7b375076cfb965d367c9a37
sridevi2009 <tiruveedulasridevi8247477004@gmail.com>
2023-12-19 00:29:34 +0530

    roboshop-shell-script

--- catalogue.sh
+++ catalogue.sh
@@ -16,6 +16,7 @@ VALIDATE(){
     if [ $1 -ne 0 ]
     then 
        echo -e "$2 ... $R FAILED $N"
+       exit 1
     else
        echo -e "$2 ... $G success $N"
     fi      
@@ -41,11 +42,19 @@ dnf install nodejs -y &>> $LOGFILE
 
 VALIDATE $? "installing nodejs:18"
 
+id roboshop
+if [ $? -ne 0 ]
+then 
+    useradd roboshop 
+    VALIDATE $? "roboshop user creation"
+else
+    echo -e "roboshop user already exist $Y skipping $N"
+fi
 useradd roboshop 
 
 VALIDATE $? "creating roboshop"
 
-mkdir /app
+mkdir -p /app
 
 VALIDATE $? "creating app directory"
 
@@ -55,7 +64,7 @@ VALIDATE $? "downloading catalogue application"
 
 cd /app
 
-unzip /tmp/catalogue.zip &>> $LOGFILE
+unzip -o /tmp/catalogue.zip &>> $LOGFILE
 
 VALIDATE $? "unzipping catalogue"
 
