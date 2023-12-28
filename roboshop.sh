166736e9046c83c76a145a248229dc9cbd09d454
sridevi2009 <tiruveedulasridevi8247477004@gmail.com>
2023-12-28 19:54:10 +0530

    roboshop shell script

--- roboshop.sh
+++ roboshop.sh
@@ -14,7 +14,7 @@ do
        INSTANCE_TYPE="t2.micro"
    fi     
      
-     aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-0f01b9f5bb3c8f55b --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
+     aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-0f01b9f5bb3c8f55b --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0],privateIpAddress' --output text
 
 done
 
