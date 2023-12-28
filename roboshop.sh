d5d6ff13512ba59affcbf818df6d7fd541967a28
sridevi2009 <tiruveedulasridevi8247477004@gmail.com>
2023-12-28 16:40:45 +0530

    roboshop shell script

--- roboshop.sh
+++ roboshop.sh
@@ -6,13 +6,13 @@ INSTANCES=("mongodb" "mysql" "redis" "cart" "catalogue" "user" "shipping" "payme
 
 for i in "${INSTANCES[@]}"
 do
-     echo "instace is: $i"
-    if [ $i == "mongodb" ] [ $i == "mysql" ] [ $i == "shipping" ]
-    then
-       INSTANCES_TYPE="t3.small"
-    else
+   echo "instace is: $i"
+   if [ $i == "mongodb" ] [ $i == "mysql" ] [ $i == "shipping" ]
+   then
+     INSTANCES_TYPE="t3.small"
+   else
        INSTANCE_TYPE="t2.micro"
-    fi     
+   fi     
      
      aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-0f01b9f5bb3c8f55b --tag-specifications "ResourceType=instance,Tags=[{Key=Name,value=$i}]"
 
