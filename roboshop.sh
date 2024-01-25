7e4d616519c6c8212bf24bf88ac6e0ba70b585b0
sridevi2009 <tiruveedulasridevi8247477004@gmail.com>
2024-01-25 17:04:01 +0530

    test

--- roboshop.sh
+++ roboshop.sh
@@ -1,45 +1,40 @@
 #!/bin/bash
+
 AMI=ami-0f3c7d07486cad139
-SGI=sg-0f01b9f5bb3c8f55b
-INSTANCES=("mongodb" "mysql" "redis" "cart" "catalogue" "user" "shipping" "payment" "dispatch" "rabbitmq" "web")
-ZONE_ID=Z01257041SG6EO358I2TX
+SG_ID=sg-0a93bf989d80ff6b1 #replace with your SG ID
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+ZONE_ID=Z01257041SG6EO358I2TX # replace your zone ID
 DOMAIN_NAME="gopisri.cloud"
 
 for i in "${INSTANCES[@]}"
 do
-   if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
-   then
-     INSTANCE_TYPE="t3.small"
-   else
-       INSTANCE_TYPE="t2.micro"
-   fi     
-     
-   IP_ADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-0f01b9f5bb3c8f55b --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
-   echo "$i: $IP_ADDRESS"
-   aws route53 change-resource-record-sets \
-  --hosted-zone-id $ZONE_ID \
-  --change-batch '
-  {
-    "Comment": "Testing creating a record set"
-    ,"Changes": [{
-      "Action"              : "UPSERT"
-      ,"ResourceRecordSet"  : {
-        "Name"              : "'$i'.'$DOMAIN_NAME'"
-        ,"Type"             : "A"
-        ,"TTL"              : 1
-        ,"ResourceRecords"  : [{
-            "Value"         : "'$IP_ADDRESS'"
+    if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
+    then
+        INSTANCE_TYPE="t3.small"
+    else
+        INSTANCE_TYPE="t2.micro"
+    fi
+
+    IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+    echo "$i: $IP_ADDRESS"
+
+    #create R53 record, make sure you delete existing record
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Creating a record set for cognito endpoint"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$i'.'$DOMAIN_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP_ADDRESS'"
+            }]
+        }
         }]
-      }
-    }]
-  }
-  '
-   
-
-
-
-
+    }
+        '
 done
-
-
-
